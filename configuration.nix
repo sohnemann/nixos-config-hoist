e139ab8da858ad24aa99af6f52f93957e0cc1378
sohnemann <50672942+sohnemann@users.noreply.github.com>
2024-03-19 19:18:54 +1300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -67,19 +67,21 @@
   # users.users.alice = {
   #   isNormalUser = true;
   #   extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
-     packages = with pkgs; [
-       git
-       vim
-       tree
-     ];
+  #   packages = with pkgs; [
+  #     git
+  #     vim
+  #     tree
+  #   ];
   # };
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
+   environment.systemPackages = with pkgs; [
+     git
+     vim
+     tree
+     wget
+  ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
