1db81977382f28f5d7cc2d1e9c13334759ebd0fd
sohnemann <50672942+sohnemann@users.noreply.github.com>
2024-03-19 20:30:16 +1300

    Update xorg.nix

--- xorg.nix
+++ xorg.nix
@@ -15,7 +15,7 @@ let
     --disable-settings-window \
     --disk-cache-dir=/dev/null \
     --disk-cache-size=1 \
-    file://home/kiosk/index.html &  
+    file:////home/kiosk/index.html &  
   '';
 
   inherit (pkgs) writeScript;
