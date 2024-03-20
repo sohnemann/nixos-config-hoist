451e76a670f488353f1e4a949fa5c86def92826e
sohnemann <50672942+sohnemann@users.noreply.github.com>
2024-03-21 11:57:50 +1300

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -15,16 +15,10 @@ in
      (import "${home-manager}/nixos")
     ];
 
-  # Arion works with Docker, but for NixOS-based containers, you need Podman
-  # since NixOS 21.05.
-  virtualisation.docker.enable = false;
-  virtualisation.podman.enable = true;
-  virtualisation.podman.dockerSocket.enable = true;
-  virtualisation.podman.defaultNetwork.dnsname.enable = true;
 
-  # Use your username instead of `myuser`
-  #users.extraUsers.kiosk.extraGroups = ["podman"];
 }
+
+
 
 
   # stop screen from going blank or turning off - needs home-manager
@@ -105,6 +99,17 @@ in
  #   # If you use docker
  #    after = ["docker.service" "docker.socket"];
  #};
+
+  # Arion works with Docker, but for NixOS-based containers, you need Podman
+  # since NixOS 21.05.
+  virtualisation.docker.enable = false;
+  virtualisation.podman.enable = true;
+  virtualisation.podman.dockerSocket.enable = true;
+  virtualisation.podman.defaultNetwork.dnsname.enable = true;
+
+  # Use your username instead of `myuser`
+  #users.extraUsers.kiosk.extraGroups = ["podman"];
+
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
